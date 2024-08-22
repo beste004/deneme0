9c58c446eeac86128285c637eb559258245f46b1
beste004 <besteren2004@hotmail.com>
2024-08-23 00:22:27 +0300

    Initial Commit

---
+++ deneme0/deneme0App.swift
@@ -0,0 +1,17 @@
+//
+//  deneme0App.swift
+//  deneme0
+//
+//  Created by Beste Eren on 23.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct deneme0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
